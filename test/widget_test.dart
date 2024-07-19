441ffbb16d34c9e991d2ad9d095ddc151e342d85
Minh Bao <74720131+bebaoboy@users.noreply.github.com>
2024-07-19 18:48:35 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,8 +13,7 @@ import 'package:airline_tag_ui/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
+    
     // Verify that our counter has incremented.
     expect(true, true);
     
